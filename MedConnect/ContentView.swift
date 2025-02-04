0f583a27d2cc4e0bfb4565e8e317b77bda77e3c6
realINL <147776788+realINL@users.noreply.github.com>
2025-02-04 19:56:58 +0300

    Initial Commit

---
+++ MedConnect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedConnect
+//
+//  Created by Илья Лебедев on 04.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedConnect/MedConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedConnectApp.swift
+//  MedConnect
+//
+//  Created by Илья Лебедев on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MedConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
